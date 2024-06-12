fd1c19e11f478196022e44a963de788153a4e3a6
Irina Anokhina <is.anohina.is@gmail.com>
2024-06-12 14:32:06 +0300

    Revert "Проект по Page Object"
    This reverts commit 1e8d318ddd3254064e654c318b9b6eb904141d7b.

--- locators/dzen_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class TestDzenLocators:
-    search_button_yandex = [By.XPATH, ".//*[@alt='Yandex']"]
-    search_main_yandex = [By.XPATH, ".//*[@data-testid='logo']"]
--- locators/transition_main_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class TestTransitionLocators:
-    search_button_scooter = [By.XPATH, ".//a[contains(@class, 'Header_LogoScooter')]"]
-    search_main_page = [By.XPATH, ".//*[text()='Привезём его прямо к вашей двери,']"]
